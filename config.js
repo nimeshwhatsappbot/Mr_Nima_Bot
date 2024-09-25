523aaa448d57f8aa7c59ae6429029a37f6a42b93
MR NIMA OFC <112799888+nimeshwhatsappbot@users.noreply.github.com>
2024-09-25 11:59:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
-MONGODB: process.env.MONGODB || "entar mongo db url",
+SESSION_ID: process.env.SESSION_ID || "EqlQhIYA#3FuS21dKYa6DM0byPKc_8-Lvp5zHf5wMKXx4GWjamRY",
+MONGODB: process.env.MONGODB || "mongodb+srv://nimeshchathurnga:Nimesh123@@cluster0.dtrhu.mongodb.net/",
 }:
